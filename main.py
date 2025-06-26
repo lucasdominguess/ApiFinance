2d4f6f28dd9bb07fc60a85cc0ecd504dd519c476
lucas <lucasdominguesofficial@gmail.com>
2025-06-26 14:16:33 -0300

    refatorando buscas

--- main.py
+++ main.py
@@ -1,90 +1,63 @@
 import yfinance as yf
+import json
+import csv
 import os
 from telegrambot import TelegramBot
 import dotenv
 from log_config import setup_logging
 
-logger = setup_logging("telegrambot")
+logger = setup_logging("main")
 dotenv.load_dotenv()
 keyApi = os.getenv('API_TOKEN')
+preco_alvo = int(os.getenv('PRICE_SEARCH', 5))  # Valor alvo para o preço das ações
+acoes_abaixo_10_json = {}
+acoes_abaixo_10_csv = []
 
-tickets = [ 
-        "PETR4.SA",
-        "SNLG11.SA"
-        "KEPL3.SA",
-        "SEQL3.SA",
-        "CASH3.SA",
-        "DASA3.SA",
-        "RNEW4.SA",
-        "CAML3.SA",
-        "HAPV3.SA",
-        "JSLG3.SA",
-        "MTRE3.SA",
-        "BPAN4.SA"
-    ]
-
-tickers2 = [
-     "ITUB3.SA",
-    "ITUB4.SA",
-    "CXSE3.SA",
-    "VULC3.SA",
-    "MTRE3.SA",
-    "JHSF3.SA",
-    "MDIA3.SA",
-    "BEES3.SA",
-    "BEES4.SA",
-    "ITSA3.SA",
-    "ITSA4.SA",
-    "BBDC4.SA",
-    "ITUB4.SA",
-    "BBDC3.SA",
-    "QUAL3.SA",
-    "CRFB3.SA",
-    "MTRE3.SA",
-    "VULC3.SA",
-    "MDNE3.SA",
-    "ALOS3.SA",
-    "MDIA3.SA",
-    "VIVT3.SA",
-    "JHSF3.SA",
-    "ITSA4.SA",
-    "ITUB4.SA",
-    "BBDC4.SA",
-    "PETR4.SA",
-    "BBAS3.SA",    
-    "KEPL3.SA",
-    "SEQL3.SA",
-    "CASH3.SA",
-    "DASA3.SA",
-    "RNEW4.SA",
-    "CAML3.SA",
-    "HAPV3.SA",
-    "JSLG3.SA",
-    "BPAN4.SA",
-    ]   
 try:
- 
+    with open("acoes.csv", "r", encoding="utf-8") as f:
+        tickets = csv.reader(f)
+        tickets = [row[0] for row in tickets if row]  # Extrai apenas os tickers
+except FileNotFoundError:
+    print("Arquivo 'acoes.csv' nao encontrado.")
+    logger.error("Arquivo 'acoes.csv' nao encontrado.")
+
+
+try:
     bot = TelegramBot()
     print("Iniciando busca das ações")
     logger.info("Iniciando busca das ações")
-    for tick in tickers2:
-        Ticker = yf.Ticker(tick)
+
+    for tick in tickets:
+        Ticker = yf.Ticker(tick)  # Adiciona .SA para ações brasileiras
         hist = Ticker.history(period="1mo", interval="1d")
         info = Ticker.info
         Price = info.get("regularMarketPrice")
-        
-        print(f' Ticker: {tick}  Preço atual R$: {Price} \n')
+        if Price is None:
+            logger.error(f"Preço não encontrado para o ticker: {tick}")
+            continue
+        if isinstance(Price, (int, float)) and (Price <= preco_alvo or tick == "PETR4.SA"):
+       
+            msg = f'Ticker: {tick} Preço atual R$:{Price} \n'
+            # print(msg)
+            acoes_abaixo_10_json[tick] = Price
+            # bot.send_message(f' Ticker: {tick} Preço atual R$:{Price} \n')
+            
+            # Adiciona os dados ao arquivo json
+            with open("acoes_abaixo_10.json", "w", encoding="utf-8") as f:
+                json.dump(acoes_abaixo_10_json, f, ensure_ascii=False, indent=4)
 
-        if Price <= 10 or tick == "PETR4.SA":
-            bot.send_message(f'Ticker: {tick} Preço atual R$:{Price} \n')
-            print(f' Ticker: {tick}  Preço atual R$: {Price} \n')
-        else:
-            print(f' Ticker: {tick}  Preço atual R$: {Price} \n')
-        
-        # bot.send_message(f' Ticker: {tick} Preço atual R$:{Price} \n')
+            # Adiciona os dados ao arquivo csv
+            acoes_abaixo_10_csv.append((tick, Price))
+            with open("acoes_abaixo_10.csv", "w", newline="", encoding="utf-8") as f:
+                writer = csv.writer(f)
+                # writer.writerow(["ticker", "preco"])  # cabeçalho
+                writer.writerows(acoes_abaixo_10_csv)
+
+        # else:
+        #     print(f' Ticker: {tick}  Preço atual R$: {Price} \n')
+    logger.info(f"Tickets abaixo de R${preco_alvo} adicionados aos arquivos JSON e CSV")
     logger.info("Busca das ações concluida")
-    print("Busca das ações concluida")
 except Exception as e:
-             print(f"Erro ao obter informações da ação: {Ticker}\n Detalhes do erro: {e}")
+            #  print(f"Erro ao obter informações da ação: {Ticker}\n Detalhes do erro: {e}")
              logger.error(f"Erro ao obter informações da ação: {Ticker} \n Detalhes do erro: {e}")
-             bot.send_message(f"Erro ao obter informações da ação: {Ticker}\nDetalhes do erro: {e}")
+            #  bot.send_message(f"Erro ao obter informações da ação: {Ticker}\nDetalhes do erro: {e}")
